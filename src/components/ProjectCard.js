13c2a9e166d99eeb128d6084bc158dce1899117e
muhammadalsattar <muhammadalsattar@gmail.com>
2021-11-29 10:11:32 +0200

    Setup projects component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
         <div className="col-sm-6 mb-3">
               <div className="card">
                 <div className="card-body">
-                  <h4 className="card-title">{props.name}<a href="#" className="btn btn-link"><BiLinkExternal/></a></h4>
+                  <h4 className="card-title">{props.name}<a href="#" className="btn btn-link align-baseline"><BiLinkExternal/></a></h4>
                   <p className="card-text">{props.details}</p>
                 </div>
                 <div className="card-footer">
