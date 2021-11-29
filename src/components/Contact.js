00576ed3a7ef809cd18f702ce66e9a455ed0d54c
muhammadalsattar <muhammadalsattar@gmail.com>
2021-11-29 09:45:30 +0200

    Setup emailjs to send contact form messages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,41 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
+class Contact extends React.Component {
+  
+  sendMail = (e) => {
+    // Sending the email
+    e.preventDefault();
+    emailjs.sendForm('service_ttc7wh9', 'template_nr72snt', e.target, 'user_vJ148TvsCvpdl2zU3QhPt')
 
-const Contact = () => (
-    <div className="section contact">
+    // Wiping form fields value
+    e.target.username.value = ''
+    e.target.useremail.value = ''
+    e.target.message.value = ''
+  }
+  render(){
+    return (
+      <div className="section contact">
           <h2>Contact</h2>
-          <form className="contact-form">
+          <form className="contact-form" onSubmit={this.sendMail}>
             <div className="form-floating mb-3">
-              <textarea className="form-control rounded-0" placeholder="Leave a message here" id="floatingTextarea"></textarea>
+              <textarea className="form-control rounded-0" name="message" placeholder="Leave a message here" id="floatingTextarea"></textarea>
               <label htmlFor="floatingTextarea">Message</label>
             </div>
             <div className="contact-inline">
               <div className="form-floating">
-                <input type="text" className="form-control rounded-0" id="floatingInput" placeholder="name@example.com"></input>
+                <input type="text" className="form-control rounded-0" name="username" id="floatingInput" placeholder="name@example.com"></input>
                 <label htmlFor="floatingInput">Full Name</label>
               </div>
               <div className="form-floating">
-                <input type="email" className="form-control rounded-0" id="floatingPassword" placeholder="Password"></input>
+                <input type="email" className="form-control rounded-0" name="useremail" id="floatingPassword" placeholder="Password"></input>
                 <label htmlFor="floatingPassword">Email Address</label>
               </div>
               <button type="submit" className="btn btn-light rounded-0">Submit</button>
             </div>
           </form>
         </div>
-)
+    )
+  }
+}
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import 'normalize.css/normalize.css'
 import './styles/style.scss'
 import Portfolio from './components/Portfolio'
 
-
 ReactDOM.render(<Portfolio/>, document.querySelector('main'))
