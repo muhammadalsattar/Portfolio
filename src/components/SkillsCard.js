28512258d81a1f2abf5a34f4382e132138bf522e
muhammadalsattar <muhammadalsattar@gmail.com>
2021-12-09 21:24:26 +0200

    initial commit

---
+++ src/components/SkillsCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SkillsCard(props) {
+    return(
+        <div className="col-sm-4 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title"><mark>{props.title}</mark></h4>
+                  <small className="card-text"><mark>{props.level}</mark></small>
+                </div>
+              </div>
+            </div>
+    )
+}
