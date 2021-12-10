7f1e84ea46375edc8ca914b00508c2c97e9ca782
muhammadalsattar <muhammadalsattar@gmail.com>
2021-12-10 13:02:07 +0200

    Fix favicon on server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const express = require('express')
 const app = express()
 const staticPath = path.join(__dirname, '..', 'public/static')
 const port = process.env.PORT || 3000
+const favicon = require('serve-favicon')
+
 
 app.use(express.static(staticPath))
+app.use(favicon(path.join(__dirname, '..', '/public/favicon.ico')))
+
 
 app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+    res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+
 })
 
 app.listen(port, () => {
